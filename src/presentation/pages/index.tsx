0cbf81081bde15374b2fc6accad688adfbdd6a51
Shubham <shubham.goapptiv@gmail.com>
2022-08-01 16:31:45 +0530

    stack and container commit

--- src/presentation/pages/index.tsx
+++ src/presentation/pages/index.tsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import GaBox from '../ga-components/Box/ga-box';
+import { Divider } from "@mui/material";
+import React from "react";
+import GaBox from "../ga-components/Box/ga-box";
+import ContainerWidth from "../ga-components/Container/container-width";
+import GaContainer from "../ga-components/Container/GaContainer";
+import GaStack from "../ga-components/Stack/GaStack";
+import StackDirection from "../ga-components/Stack/stack-direction";
 
 const Index = () => {
   return (
     <div>
-        <GaBox>
-            <div>
-                <p>
-                    helllo
-                </p>
-            </div>
-            <div>
-                <p>
-                    helllo
-                </p>
-            </div>
-        </GaBox>
-    </div>
-  )
-}
+      <GaContainer maxWidth={ContainerWidth.XL} disableGutters>
+        <GaStack spacing={2} direction={StackDirection.ROW} divider={<Divider orientation="vertical" flexItem/>}>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </GaStack>
 
-export default Index
+        <GaBox>
+        </GaBox>
+      </GaContainer>
+
+    </div>
+  );
+};
+
+export default Index;
